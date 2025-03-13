09addb47c7ffe9841f4fe7e7db3d7007c07ace24
KirmadaShukla <kirmadashukla@gmail.com>
2025-03-13 12:16:54 +0530

    Modifications

--- backend/routes/converRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { convertToFigma } = require('../controllers/converterController');
-const router = express.Router();
-
-router.post('/convert', convertToFigma);
-
-module.exports = router;
